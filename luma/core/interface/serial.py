0b83b1833577455bc97b3b48bd713c0bf097098c
chaosAD <frasker@hotmail.com>
2021-03-10 01:26:21 +0800

    Faking i2c interface

--- luma/core/interface/serial.py
+++ luma/core/interface/serial.py
@@ -49,31 +49,12 @@ class i2c(object):
           that it has already been opened.
     """
     def __init__(self, bus=None, port=1, address=0x3C):
-        import smbus2
-        self._cmd_mode = 0x00
-        self._data_mode = 0x40
-
         try:
             self._addr = int(str(address), 0)
         except ValueError:
             raise luma.core.error.DeviceAddressError(
                 f'I2C device address invalid: {address}')
-
-        try:
-            self._managed = bus is None
-            self._i2c_msg_write = smbus2.i2c_msg.write if bus is None else None
-            self._bus = bus or smbus2.SMBus(port)
-        except (IOError, OSError) as e:
-            if e.errno == errno.ENOENT:
-                # FileNotFoundError
-                raise luma.core.error.DeviceNotFoundError(
-                    f'I2C device not found: {e.filename}')
-            elif e.errno in [errno.EPERM, errno.EACCES]:
-                # PermissionError
-                raise luma.core.error.DevicePermissionError(
-                    f'I2C device permission denied: {e.filename}')
-            else:  # pragma: no cover
-                raise
+        print(f'Creating i2c on port {port} with target address of {address}')
 
     def command(self, *cmd):
         """
@@ -86,17 +67,6 @@ class i2c(object):
         """
         assert(len(cmd) <= 32)
 
-        try:
-            self._bus.write_i2c_block_data(self._addr, self._cmd_mode,
-                                           list(cmd))
-        except (IOError, OSError) as e:
-            if e.errno in [errno.EREMOTEIO, errno.EIO]:
-                # I/O error
-                raise luma.core.error.DeviceNotFoundError(
-                    'I2C device not found on address: 0x{0:02X}'.format(self._addr))
-            else:  # pragma: no cover
-                raise
-
     def data(self, data):
         """
         Sends a data byte or sequence of data bytes to the I²C address.
@@ -108,37 +78,18 @@ class i2c(object):
         :param data: A data sequence.
         :type data: list, bytearray
         """
-
-        # block size is the maximum data payload that will be tolerated.
-        # The managed i2c will transfer blocks of upto 4K (using i2c_rdwr)
-        # whereas we must use the default 32 byte block size when unmanaged
-        if self._managed:
-            block_size = 4096
-            write = self._write_large_block
-        else:
-            block_size = 32
-            write = self._write_block
-
-        i = 0
-        n = len(data)
-        while i < n:
-            write(list(data[i:i + block_size]))
-            i += block_size
+        pass
 
     def _write_block(self, data):
         assert len(data) <= 32
-        self._bus.write_i2c_block_data(self._addr, self._data_mode, data)
+        pass
 
     def _write_large_block(self, data):
         assert len(data) <= 4096
-        self._bus.i2c_rdwr(self._i2c_msg_write(self._addr, [self._data_mode] + data))
+        pass
 
     def cleanup(self):
-        """
-        Clean up I²C resources
-        """
-        if self._managed:
-            self._bus.close()
+        pass
 
 
 @lib.rpi_gpio
